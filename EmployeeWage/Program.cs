26c5ebad94dab7dbc8eb28df4947104cba30a32c
vibha <vibhauc@gmail.com>
2022-02-14 23:08:16 +0530

    [Vibha] Using switch case

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -5,10 +5,13 @@ namespace EmployeeWage
 {
     class Program
     {
+       public const int fullTime = 1;
+       public const int partTime = 2;
+       //public const int empRatePerHour = 20;
         static void Main(string[] args)
         {
-            int fullTime = 1;
-            int partTime = 2;
+            //int fullTime = 1;
+            //int partTime = 2;
             int empRatePerHour = 20;
 
             int empHour = 0;
@@ -17,18 +20,18 @@ namespace EmployeeWage
             Random random = new Random();
 
             int empCheck = random.Next(0,3);
-            if (empCheck == partTime)
+            switch (empCheck)
             {
-                empHour = 4;
-            }
-            else if (empCheck == fullTime)
-            {
-                empHour = 8;
-            }
-            else
-            {
-                empHour = 0;
-            }
+                case partTime:
+                    empHour = 4;
+                    break;
+                case fullTime:
+                    empHour = 8;
+                    break;
+                default:
+                    empHour = 0;
+                    break;
+           }
             empWage = empHour * empRatePerHour;
             Console.WriteLine("Employee wage : " + empWage);
         }
