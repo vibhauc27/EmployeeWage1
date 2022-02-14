a83e230d88431b9f1919ea7563b611d6b58ccf66
vibha <vibhauc@gmail.com>
2022-02-14 22:05:11 +0530

    Initial commit

---
+++ EmployeeWage/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EmployeeWage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fullTime = 1;
+            Random random = new Random();
+
+            int empCheck = random.Next(0,2);
+            if (empCheck == fullTime)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+            
+        }
+    }
+}
